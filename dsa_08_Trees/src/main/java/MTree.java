7cfe9ff0e42a532d5636e63d9cdf4c86a574d8b4
Yevgeniy <yevgengeodakyan@gmail.com>
2024-06-23 12:46:24 -0400

    methods in MTree added

--- dsa_08_Trees/src/main/java/MTree.java
+++ dsa_08_Trees/src/main/java/MTree.java
@@ -78,8 +78,54 @@ public class MTree {
         while (curr != null) {
             if (val < curr.val) curr = curr.left;
             else if (val > curr.val) curr = curr.right;
-            else return true;
+            else {
+                if (isLeaf(curr)) System.out.println("It is a Leaf");
+                return true;
+            }
         }
         return false;
+    }
+
+
+    boolean isLeaf(TNode node) {
+        return node.left == null && node.right == null;
+    }
+
+    public void printLeaves(TNode root) {
+        if (root == null) return;
+        printLeaves(root.left);
+        printLeaves(root.right);
+        if (isLeaf(root)) System.out.print(root.val + ", ");
+    }
+
+    int height(TNode root) {
+        if (root == null) return -1;
+        if (isLeaf(root)) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+
+    int sumOfNodes(TNode root) {
+        if (root == null) return 0;
+        return root.val + sumOfNodes(root.left) + sumOfNodes(root.right);
+
+    }
+
+    int countLeaves(TNode root) {
+        if (root == null) return 0;
+        if (isLeaf(root)) return 1;
+        return countLeaves(root.left) + countLeaves(root.right);
+    }
+
+    int findSomeOfLeaves(TNode root) {
+        if (root == null) return 0;
+        if (isLeaf(root)) return root.val;
+        return findSomeOfLeaves(root.left) + findSomeOfLeaves(root.right);
+    }
+    public int calculateNodeSums(){
+        return nodeSums(root);
+    }
+    int nodeSums(TNode root) {
+        if (root == null) return 0;
+        return root.val + nodeSums(root.left) + nodeSums(root.right);
     }
 }
--- dsa_08_Trees/src/main/java/TreeApp.java
+++ dsa_08_Trees/src/main/java/TreeApp.java
@@ -2,7 +2,7 @@ public class TreeApp {
     public static void main(String[] args) {
 
         MTree tree = new MTree();
-        int[] nums = {10, 6, 8, 20, 4, 9, 5, 17, 42, 47, 29};
+        int[] nums = {10, 6, 8, 20};
         for (int num : nums) {
             tree.insert(num);
         }
@@ -17,7 +17,14 @@ public class TreeApp {
         System.out.println("\nLevel-Order traversal of the Tree");
         tree.levelOrderTraversal(tree.root);
         System.out.println();
-        System.out.println(tree.contains(42));
-        System.out.println(tree.contains(23));
+        System.out.println(tree.contains(8));
+        System.out.println();
+        tree.printLeaves(tree.root);
+        System.out.println();
+        System.out.println("Height of the root : " + tree.height(tree.root));
+        System.out.println("Sum of tree nodes : " + tree.sumOfNodes(tree.root));
+        System.out.println("Amount of leaves : " + tree.countLeaves(tree.root));
+        System.out.println("Sum of leaves : " + tree.findSomeOfLeaves(tree.root));
+        System.out.println("Sum of nodes with helper : " + tree.calculateNodeSums());
     }
 }
