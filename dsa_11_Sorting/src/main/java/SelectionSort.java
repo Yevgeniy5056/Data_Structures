799d5f5b2e7d636d2ca71a70498c341900af7544
Yevgeniy <yevgengeodakyan@gmail.com>
2024-07-08 10:17:13 -0400

    SelectionSort example

---
+++ dsa_11_Sorting/src/main/java/SelectionSort.java
@@ -0,0 +1,22 @@
+public class SelectionSort {
+
+    static int[] selectionSort(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIdx]) {
+                    minIdx = j;
+                }
+                swap(array, minIdx, i);
+            }
+        }
+        return array;
+    }
+
+    static void swap(int[] array, int idx1, int idx2) {
+        int temp = array[idx1];
+        array[idx1] = array[idx2];
+        array[idx2] = temp;
+    }
+}
